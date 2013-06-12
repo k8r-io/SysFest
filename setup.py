a283094ac4a5aaf80c828db37b4dce3736f90c76
Kristopher Kirkland <kirkland@umn.edu>
2013-06-12 11:08:57 -0500

    Fixed error in setup script
    sysfest.config was removed in previous changes to how configuration
    works

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version='0.1.1',
 	author='Kristopher Kirkland',
 	author_email='kirkland@umn.edu',
-	packages=['sysfest','sysfest.data','sysfest.config'],
+	packages=['sysfest','sysfest.data'],
 	url='https://github.com/ASCIIDuck/SysFest',
 	description='Backend CRUD api used for tracking host inventory',
 	install_requires=[
