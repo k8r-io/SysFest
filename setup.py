f3cd309d93d75489a4e8625277e2a99b0e054cef
Kristopher Kirkland <kirkland@umn.edu>
2013-07-24 16:30:07 -0500

    Merge branch 'master' into development

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
 	name='SysFest',
-	version='0.1.0',
+	version='0.1.1',
 	author='Kristopher Kirkland',
 	author_email='kirkland@umn.edu',
-	packages=['sysfest','sysfest.data','sysfest.config'],
+	packages=['sysfest','sysfest.data'],
 	url='https://github.com/ASCIIDuck/SysFest',
 	description='Backend CRUD api used for tracking host inventory',
 	install_requires=[
