76a7c4e5f3cd7772d6085910ffb26b61554673c5
Kristopher Kirkland <kirkland@umn.edu>
2013-08-26 15:32:00 -0500

    Updated build version info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='SysFest',
-	version='0.1.1',
+	version='0.1.2',
 	author='Kristopher Kirkland',
 	author_email='kirkland@umn.edu',
 	packages=['sysfest','sysfest.data'],
